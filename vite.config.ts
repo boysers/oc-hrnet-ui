7fb75027372d24735cb71c48b92b12ee177720bf
boysers <boyser2015@gmail.com>
2023-11-26 09:09:18 +0100

    chore: build config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
+import { resolve } from "node:path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [dts({ insertTypesEntry: true })],
+	build: {
+		lib: {
+			entry: resolve(__dirname, "src/index.ts"),
+			name: "oc-hrnet-ui",
+			fileName: "oc-hrnet-ui",
+		},
+		rollupOptions: {
+			external: ["react", "react-dom"],
+			output: {
+				globals: {
+					react: "React",
+					"react-dom": "ReactDOM",
+				},
+			},
+		},
+	},
+});
