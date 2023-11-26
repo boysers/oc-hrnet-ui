380c9a35824b72cbc4a38f19dc45c35cc8cee6d4
boysers <boyser2015@gmail.com>
2023-11-26 09:34:11 +0100

    chore: fix cp package.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import dts from "vite-plugin-dts";
 import { resolve } from "node:path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [dts({ insertTypesEntry: true })],
+	plugins: [react(), dts({ insertTypesEntry: true })],
 	build: {
 		lib: {
 			entry: resolve(__dirname, "src/index.ts"),
